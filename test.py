7cfee295554fe8c964247caaaa3b7cd83e91a933
tangoyankee <miller.tim108@gmail.com>
2018-06-14 23:27:12 -0700

    more robust database management

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ item = {
     'values': [0.098, 0.872762, 0.12, 0.563]
 }
 
-testPipeline = hello_world.BasePipeline()
-saved_item = testPipeline.process_cancer(item)
-testPipeline.query_database()
+# testPipeline = hello_world.BasePipeline()
+# saved_item = testPipeline.process_cancer(item)
+# testPipeline.query_database()
+# testPipeline.reset()
+# testPipeline.query_database()
