3ccdd8c9678deb60026d611d6c1b5eec44d5124f
tangoyankee <tangoyankee>
2018-09-28 08:25:56 -0700

    add file for script specific to initially loading data into sql database

---
+++ neural_network/load_data.py
@@ -0,0 +1,64 @@
+"""
+Define the SQL Data Session and control the flow of the Neural Network
+"""
+from contextlib import contextmanager
+import os
+import csv
+import sys
+sys.path.insert(0, './')
+import settings
+import models as mdls
+from sqlalchemy.orm import sessionmaker
+
+ENGINE = mdls.db_connect()
+SESSION = sessionmaker(bind=ENGINE)
+
+
+@contextmanager
+def session_scope():
+    """ Control the SQL Database Session  """
+    session = SESSION()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+    finally:
+        session.close()
+
+def get_from_csv(file_name):
+    """Before the database is initialized, data are stored in csv files. 
+       This function is called by def load_data"""
+    data_open = open(os.path.join(settings.DATA_DIR, file_name))
+    return csv.DictReader(data_open)
+
+def check_database(file_name):
+    if file_name == "cancer_data.csv":
+        database = "cancerdata"
+    elif file_name == "ep_data.csv":
+        database = "epdata"
+    elif file_name == "pnn_data.csv":
+        database = "pnndata"
+    else:
+        sys.exit('data file does not match existing database')
+    return database
+
+##TODO: 
+# 1. Function to make values floats and true values integers
+# 2. Function to add row of data to sql database  
+#   - Will need way to generalize models in add functions
+# 3. Standardized way to track and check file names, avoiding hard-coding of file names outside of settings 
+# 4. Standardized way to track names of tables and link to file names
+
+def load_data():
+    """  Control the flow of the Neural Network """
+    with session_scope() as session:
+        file_name = settings.DATA_FILE_NAMES[0]
+        database = check_database(file_name)
+        dataDict = get_from_csv(file_name)
+        for data in dataDict:
+            float_value = float(data["val_one"])
+            print (type(float_value))
+            print (float_value)
+        
+load_data()
