4304fd79b3436ab930e586c54cd17ab16a004972
tangoyankee <tangoyankee>
2018-10-01 16:37:55 -0700

    implement function in main to ensure data loaded correctly

--- neural_network/main.py
+++ neural_network/main.py
@@ -3,12 +3,21 @@ Define the SQL Data Session and control the flow of the Neural Network
 """
 import models as mdls
 
-K_FOLDS = 5
+# K_FOLDS = 5
 
 def main():
     """  Control the flow of the Neural Network """
     with mdls.session_scope() as session:
-        print("hello, session")
+        print("Cancer")
+        for each_item in session.query(mdls.CancerData).all():
+            print(each_item.id, each_item.var_one, each_item.fold)
+        print("EP")
+        for each_item in session.query(mdls.EpData).all():
+            print(each_item.id, each_item.var_one, each_item.fold)
+        print("PNN")
+        for each_item in session.query(mdls.PnnData).all():
+            print(each_item.id, each_item.var_one, each_item.fold)
+        
         # print("Clear Data")
         # Pipeline().clear_data(session)
         # print("Load Data")
@@ -19,6 +28,7 @@ def main():
         # print("clear folds")
         # Pipeline().clear_folds(session)
         # Pipeline().print_all(session)
+
 
 if __name__ == "__main__":
     main()
